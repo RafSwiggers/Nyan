fc34338b51f274ba7f7fa7dfb2b7ab2f5e9cc4e8
RafSwiggers <rafswiggers@gmail.com>
2019-05-28 16:17:55 +0200

    attempt two at the same time

--- script/script.js
+++ script/script.js
@@ -1,13 +1,37 @@
 function nyan() {
     var pos = 0;
     var id = setInterval(frame, 50);
+    var id = setInterval(frame2, 50);
     var im = document.createElement("img");
     im.src = "/Nyan/images/Nyan.png";
     document.body.appendChild(im);
     im.id = "kitty";
+    var im2 = document.createElement("img");
+    im2.src = "/Nyan/images/Nyan.png";
+    document.body.appendChild(im2);
+    im2.id = "kitty";
 
 
     function frame() {
+        document.getElementById("button").style.display = "none";
+        document.getElementById("song").play();
+        document.body.style.background = "url('/Nyan/images/background-gif.gif')";
+
+
+        if (pos == 100) {
+            clearInterval(id);
+            document.getElementById("button").style.display = "block";
+            document.getElementById("song").pause();
+            document.body.style.background = "none";
+
+        } else {
+            pos++;
+            im.style.top = pos + '%';
+            im.style.left = pos + '%';
+        }
+    }
+
+    function frame2() {
         document.getElementById("button").style.display = "none";
         document.getElementById("song").play();
         document.body.style.background = "url('/Nyan/images/background-gif.gif')";
