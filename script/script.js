200fc840de268b7de6b95dd316199b03b2737704
RafSwiggers <rafswiggers@gmail.com>
2019-05-28 14:06:39 +0200

    clean up the JS

---
+++ script/script.js
@@ -0,0 +1,32 @@
+function nyan() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 50);
+
+
+
+    function frame() {
+        document.getElementById("button").style.display = "none";
+        elem.style.display = "block";
+        document.getElementById("song").play();
+        document.body.style.background = "url('/images/background-gif.gif')";
+        elem.style.left = '5px';
+
+        if (pos == 100) {
+            clearInterval(id);
+            document.getElementById("button").style.display = "block";
+            elem.style.display = "none";
+            document.getElementById("song").pause();
+            document.body.style.background = "none";
+
+        } else {
+            pos++;
+            elem.style.top = pos + '%';
+            elem.style.left = pos + '%';
+
+
+
+
+        }
+    }
+}
