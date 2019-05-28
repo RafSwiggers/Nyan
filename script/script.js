93dd06c0860e29e716352635844c95d9d1d502da
RafSwiggers <rafswiggers@gmail.com>
2019-05-28 15:11:40 +0200

    change animation to create element in JS

--- script/script.js
+++ script/script.js
@@ -1,21 +1,22 @@
 function nyan() {
-    var elem = document.getElementById("animate");
     var pos = 0;
     var id = setInterval(frame, 50);
-
+    var im = document.createElement("img");
+    im.src = "/Nyan/images/Nyan.png";
+    document.body.appendChild(im);
+    im.id = "kitty";
+    var elem = document.getElementById("kitty")
 
 
     function frame() {
         document.getElementById("button").style.display = "none";
-        elem.style.display = "block";
         document.getElementById("song").play();
         document.body.style.background = "url('/Nyan/images/background-gif.gif')";
-        elem.style.left = '5px';
+
 
         if (pos == 100) {
             clearInterval(id);
             document.getElementById("button").style.display = "block";
-            elem.style.display = "none";
             document.getElementById("song").pause();
             document.body.style.background = "none";
 
@@ -23,10 +24,6 @@ function nyan() {
             pos++;
             elem.style.top = pos + '%';
             elem.style.left = pos + '%';
-
-
-
-
         }
     }
 }
