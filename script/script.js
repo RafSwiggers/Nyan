049b7ad190673082e33e289d624b511854850c8e
RafSwiggers <rafswiggers@gmail.com>
2019-05-29 09:37:44 +0200

    new form for code

--- script/script.js
+++ script/script.js
@@ -1,30 +1,30 @@
 function nyan() {
-    var pos = 0;
-    var id = setInterval(frame, 50);
+    let pos = 0;
+    let id = setInterval(frame, 50);
+    let im = document.createElement("img");
+    im.src = "/Nyan/images/Nyan.png";
+    document.body.appendChild(im);
+    im.id = "kitty";
+    let elem = document.getElementById("kitty")
     document.getElementById("button").style.display = "none";
     document.getElementById("song").play();
     document.body.style.background = "url('/Nyan/images/background-gif.gif')";
-    for (i = 0, i < 10, i++, ) {
-        function frame() {
+
+    function frame() {
 
 
-            if (pos == 100) {
-                clearInterval(id);
+
+        if (pos == 100) {
+            clearInterval(id);
 
 
-            } else {
-                var im = document.createElement("img");
-                im.src = "/Nyan/images/Nyan.png";
-                document.body.appendChild(im);
-                im.id = "kitty";
-                var elem = document.getElementById("kitty")
-                pos++;
-                elem.style.top = pos + '%';
-                elem.style.left = pos + '%';
-            }
+        } else {
+            pos++;
+            elem.style.top = pos + '%';
+            elem.style.left = pos + '%';
         }
-    };
+    }
     document.getElementById("button").style.display = "block";
     document.getElementById("song").pause();
     document.body.style.background = "none";
-};
+}
