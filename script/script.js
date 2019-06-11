4a2d6c3a06c7d4021bf3810e3d8cf2381d5f49ed
RafSwiggers <rafswiggers@gmail.com>
2019-06-11 10:19:43 +0200

    nyan animation

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
 function nyan() {
     var pos = 0;
-    var id = setInterval(frame, 90);
+    var id = setInterval(frame, 16);
     elem = document.getElementById("kitty")
 
 
     function frame() {
-        if (pos == 100) {
+        if (pos == 1000) {
             clearInterval(id);
             document.getElementById("button").style.display = "block";
             document.getElementById("song").pause();
             document.body.style.background = "none";
         } else {
-            pos++;
+            pos += 0.25;
             elem.style.display = "block";
             document.getElementById("button").style.display = "none";
             document.getElementById("song").play();
             document.body.style.background = "url('/images/background-gif.gif')";
             elem.style.top = pos + '%';
-            elem.style.left = pos + '%';
+            elem.style.left = pos + '%'
         }
     }
 }
