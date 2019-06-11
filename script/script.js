8dbac2246fa51636390493294afeee29faa56fe8
RafSwiggers <rafswiggers@gmail.com>
2019-06-11 10:08:41 +0200

    change function to display image

--- script/script.js
+++ script/script.js
@@ -1,29 +1,23 @@
 function nyan() {
     var pos = 0;
     var id = setInterval(frame, 90);
+    elem = document.getElementById("kitty")
 
 
     function frame() {
-        document.getElementById("button").style.display = "none";
-        document.getElementById("song").play();
-        document.body.style.background = "url('/images/background-gif.gif')";
-        for (i = 0; i < 5; i++) {
-            var im = document.createElement("img");
-            im.src = "/images/Nyan.png";
-            document.body.appendChild(im);
-            im.id = "kitty" + i;
-            im.class = "kitty";
-            var elem = document.getElementById("kitty" + i)
-            if (pos == 100) {
-                clearInterval(id);
-            } else {
-                pos++;
-                elem.style.top = pos + '%';
-                elem.style.left = pos + '%';
-            }
+        if (pos == 100) {
+            clearInterval(id);
+            document.getElementById("button").style.display = "block";
+            document.getElementById("song").pause();
+            document.body.style.background = "none";
+        } else {
+            pos++;
+            elem.style.display = "block";
+            document.getElementById("button").style.display = "none";
+            document.getElementById("song").play();
+            document.body.style.background = "url('/images/background-gif.gif')";
+            elem.style.top = pos + '%';
+            elem.style.left = pos + '%';
         }
     }
-    document.getElementById("button").style.display = "block";
-    document.getElementById("song").pause();
-    document.body.style.background = "none";
 }
